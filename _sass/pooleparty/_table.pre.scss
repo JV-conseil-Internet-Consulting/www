2865e4f59b30d9a3f099fe3c7bfb9a6fbf355c1a
JV-conseil <contact@jv-conseil.net>
2023-03-30 20:51:57 +0200

    update

--- _sass/pooleparty/_table.pre.scss
+++ _sass/pooleparty/_table.pre.scss
@@ -1,4 +1,4 @@
-// Copyright (c) 2017 JV conseil <https://www.jv-conseil.net/>
+// Copyright (c) 2023 JV conseil <https://www.jv-conseil.net/>
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
