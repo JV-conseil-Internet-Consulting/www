f9e8f7ba927a00f0d6da01a674695e5b6c50dd26
JV-conseil <contact@jv-conseil.net>
2024-03-01 00:27:50 +0100

    "packageManager": "yarn@4.1.0"

--- .bash/osx/gem.sh
+++ .bash/osx/gem.sh
@@ -3,7 +3,7 @@
 #
 # author        : JV-conseil
 # credits       : JV-conseil
-# copyright     : Copyright (c) 2019-2023 JV-conseil
+# copyright     : Copyright (c) 2019-2024 JV-conseil
 #                 All rights reserved
 #
 # bundle add github-pages jekyll-avatar jekyll-mentions --group "jekyll_plugins"
